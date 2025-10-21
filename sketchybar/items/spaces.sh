c97d347d9688277877ee70d874767a193c06ab49
etcetera <hubertetcetera@gmail.com>
2025-10-21 22:07:19 +0530

    fix(spaces): update icon font to JetBrainsMonoNL for consistency in appearance

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -27,7 +27,7 @@ done
 
 sketchybar   --add item       separator left                          \
              --set separator  icon=                                  \
-                              icon.font="Hack Nerd Font:Regular:16.0" \
+                              icon.font="JetBrainsMonoNL Nerd Font Mono:Regular:16.0" \
                               background.padding_left=15              \
                               background.padding_right=15             \
                               label.drawing=off                       \
