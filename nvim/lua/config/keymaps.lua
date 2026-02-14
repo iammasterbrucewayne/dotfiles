1ed2dac1ab42c09ece7e2417f9e670f6982c0df0
etcetera <hubertetcetera@gmail.com>
2026-02-14 10:56:46 +0530

    feat(keymaps): add keybindings for compiling and building C++ files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,13 @@
 
 -- Toggle Catppuccin light/dark (latte/Macchiato)
 vim.keymap.set("n", "<leader>ut", "<cmd>CatppuccinToggle<cr>", { desc = "Toggle light/dark theme" })
+
+-- C++: compile current file (g++) or build (make)
+vim.keymap.set("n", "<leader>cc", function()
+  local file = vim.fn.expand("%")
+  if file:match("%.cpp$") then
+    -- Run from file's directory so the binary is found; use absolute path to run it
+    vim.cmd("terminal g++ -o %:p:r % && %:p:r")
+  end
+end, { desc = "Compile and run current C++ file" })
+vim.keymap.set("n", "<leader>cb", "<cmd>make<cr>", { desc = "Build (make)" })
